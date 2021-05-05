9fb12cafba6dec6e8daeeb4e1e854f44ec6a6cb5
DaOnlyOwner <karstentorman1122@gmail.com>
2021-05-05 13:41:37 +0200

    Init

---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+// Testing if the include works
+#include "volk.h"
+#include "vulkan/vk_enum_string_helper.h"
+#include "glm/common.hpp"
+#include "tiny_gltf.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "HELLO WORLD";
+}
